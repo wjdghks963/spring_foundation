a1d49ccc93f8a9dfb96c41e08c648c0c9de9ffd1
wjdghks963 <chsw000@naver.com>
2022-12-26 21:05:06 +0900

    api

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,14 +3,57 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    // http get, template mapping => 8080/hello을 만들고 template/hello와 맵핑된것과 같다.
+    // http get => 8080/hello을 만든것과 같다.
     @GetMapping("hello")
     public String Hello(Model model){
         // templates의 data의 변수에 hello!라는 값을 초기화한다.
         model.addAttribute("data", "hello!");
+        // static의 template/hello와 맵핑된것과 같다.
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam(value="name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+ name;
+    }
+
+    // api 생성
+    // ResponseBody는 결과물을 json형태로 변환시켜 준다.
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+
+
+
 }
