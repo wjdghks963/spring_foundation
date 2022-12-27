8ae66b1cdd8fa451538a82dd3f4b6ac64d6a9c1a
wjdghks963 <chsw000@naver.com>
2022-12-27 14:06:03 +0900

    spring bean

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,2 +1,19 @@
-package hello.hellospring.controller;public class MemberController {
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+// Controller 어노테이션은 spring container에서 관리된다. => bin
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // Autowired 어노테이션은 spring container에서 memberService를 가져옴 => DI
+    @Autowired
+    public MemberController(MemberService memberService){
+        this.memberService = memberService;
+    }
+
 }
