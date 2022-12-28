ca54d8bebbc9768268de5c46e03ad7f540bddedb
wjdghks963 <chsw000@naver.com>
2022-12-28 22:35:35 +0900

    jpa basic

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,37 @@
-//package hello.hellospring;
-//
-//import hello.hellospring.repository.MemberRepository;
-//import hello.hellospring.repository.MemoryMemberRepository;
-//import hello.hellospring.service.MemberService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
+package hello.hellospring;
 
-//@Configuration
-//public class SpringConfig {
-//
-//    @Bean
-//    public MemberService memberService(){
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
-//    }
-//}
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em){
+        this.em = em;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+
+        //return new MemoryMemberRepository();
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+    }
+}
